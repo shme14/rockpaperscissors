02ac64c9ce7e177b11ca9fde46cc8c47c9e1fa5c
Jamie <16787928+shme14@users.noreply.github.com>
2024-01-04 23:00:32 -0700

    added buttons and score results

--- js.js
+++ js.js
@@ -1,11 +1,20 @@
-playGame();
-playGame();
-playGame();
-playGame();
-playGame();
+// playGame();
+// playGame();
+// playGame();
+// playGame();
+// playGame();
 
-
-
+const gameResult = document.querySelector("#gameResult");
+const resultsDiv = document.querySelector("#resultsDiv");
+const buttonRock = document.querySelector("#rock");
+const buttonPaper = document.querySelector('#paper');
+const buttonScissors = document.querySelector('#scissors');
+buttonRock.onclick = function() {playGame('rock')};
+buttonPaper.onclick = function() {playGame('paper')};
+buttonScissors.onclick = function() {playGame('scissors')};
+let gameNumber = 1;
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice(){
     let random=Math.floor(Math.random() * 3);
@@ -28,24 +37,46 @@ function getPlayerChoice(){
     return playerSelection;
 }
 
-function playGame(){
-    let playerSelection = getPlayerChoice();
+function playGame(playerSelection){
+    // let playerSelection = getPlayerChoice();
     let computerSelection = getComputerChoice();
     if (playerSelection == computerSelection){
-        console.log("It's a Tie!");
+        resultsDiv.textContent = "It's a Tie!";
     } 
     else if ((playerSelection == "rock" && computerSelection == "scissors") || 
     (playerSelection == "scissors" && computerSelection == "paper") || 
     (playerSelection == "paper" && computerSelection == "rock")) {
-        console.log(`You Win! ${playerSelection} beats ${computerSelection}`);
+        resultsDiv.textContent = `You Win! ${playerSelection} beats ${computerSelection}`;
+        playerScore++;
     }
     else if ((playerSelection == "rock" && computerSelection == "paper") || 
     (playerSelection == "scissors" && computerSelection == "rock") || 
     (playerSelection == "paper" && computerSelection == "scissors")) {
-        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
+        resultsDiv.textContent = `You Lose! ${computerSelection} beats ${playerSelection}`;
+        computerScore++;
     }
     else {
-        console.log("Invalid Player Choice");
+        resultsDiv.textContent = "Invalid Player Choice";
+    }
+    
+    if (gameNumber == 5) {
+        if (playerScore<computerScore){
+            gameResult.textContent = `PLAYER LOST! Score: Player: ${playerScore} Computer: ${computerScore}`;
+        }
+        else if (playerScore>computerScore){
+            gameResult.textContent = `PLAYER WON! Score: Player: ${playerScore} Computer: ${computerScore}`;
+        }
+        else{
+            gameResult.textContent = `PLAYER TIED! Score: Player: ${playerScore} Computer: ${computerScore}`;
+        }
+        
+        gameNumber=1;
+        computerScore=0;
+        playerScore=0;
+    }
+    else {
+        gameNumber++;
+        gameResult.textContent = `Score: Player: ${playerScore} Computer: ${computerScore}`;
     }
 
 }
